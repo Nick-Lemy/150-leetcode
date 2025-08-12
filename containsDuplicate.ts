f383510ec64f51897c763a07cc32d3cb1f0e891b
Nick-Lemy Kayiranga <169042721+Nick-Lemy@users.noreply.github.com>
2025-08-12 13:23:44 +0200

    containsDuplicate solved

---
+++ containsDuplicate.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+function containsDuplicate(nums: number[]): boolean {
+   const numsMap = new Map()
+   for(const elem of nums){
+    if(numsMap.get(elem)){
+        return true
+    }
+    numsMap.set(elem, 1)
+   }
+   return false
+};
